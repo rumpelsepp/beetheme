e346cf7d34de93756177ca0e08c8dadc319b4429
Stefan Tatschner <stefan@rumpelsepp.org>
2024-07-31 11:34:28 +0200

    Initial stuff

---
+++ assets/js/main.ts
@@ -0,0 +1,5 @@
+import 'sortable-tablesort/sortable.min.js';
+
+export { DateTime } from "luxon";
+export { renderBeeCalendar, renderTUBCalendar } from "./calendars";
+export { getParam, getDateParam } from "./helpers";
